64fd80d6047f4c03c265efcd8ce136842b4ba54d
Josh <josh@localhost.localdomain>
2024-11-14 16:21:58 -0500

    correct setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from setuptools import setup, find_packages
 
 # Safely read README.md
 long_description = ""
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "A Python Selenium framework for automation."
 
 setup(
     name="webserpent",
     version="0.1.0",
     author="Joshua Bowery",
-    description="A python selenium framework",
+    author_email="joshua@example.com",  # Add your email here for contact
+    description="A Python Selenium framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jcbowery/web_serpent",
